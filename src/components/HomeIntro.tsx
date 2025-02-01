645e124292e61ef18ee05e26e3ccdfb5a6ccfe91
Francisco Lopez <francisco.lopezmorelos01@gmail.com>
2025-01-31 20:28:07 -0800

    Fix the main text for the home page.

--- src/components/HomeIntro.tsx
+++ src/components/HomeIntro.tsx
@@ -4,10 +4,19 @@ import { Box, Text } from "@chakra-ui/react"
 const HomeIntro = () => {
   return (
     <>
-        <Box marginBottom="2rem">
+        <Box marginBottom="2rem" display={{base: "none", md: "block"}}>
             <Text textAlign="center" color="black" fontWeight="bold" textStyle="2xl" fontFamily="Lora">Hello,</Text>
             <Text textAlign="center" color="black" fontWeight="bold" textStyle="4xl" fontFamily="Lora">my name if Francisco.</Text>
             <Text textAlign="center" color="black" textStyle="3xl" fontFamily="Lora" margin="1rem 5rem">
+            I am an aspiring Front-end Developer who’s constantly expanding their knowledge base with new skills.
+            I’m inspired to create visually stunning and user friendly experiences for a polished final product.
+            My career goals revolve around the planning, designing and creation of web applications.
+            </Text>
+        </Box>
+        <Box marginBottom="2rem" display={{base: "block", md: "none"}}>
+            <Text textAlign="center" color="black" fontWeight="bold" textStyle="xl" fontFamily="Lora">Hello,</Text>
+            <Text textAlign="center" color="black" fontWeight="bold" textStyle="3xl" fontFamily="Lora">my name if Francisco.</Text>
+            <Text textAlign="center" color="black" textStyle="2xl" fontFamily="Lora" margin="1rem 5rem">
             I am an aspiring Front-end Developer who’s constantly expanding their knowledge base with new skills.
             I’m inspired to create visually stunning and user friendly experiences for a polished final product.
             My career goals revolve around the planning, designing and creation of web applications.
--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -6,7 +6,8 @@ const MyWork = () => {
   return (
     <>
         <Box boxSize={60} bgColor={'black'} width="100%" className='skewProjects' display="flex">
-            <Text marginLeft="5rem" textStyle="5xl" fontFamily="Lora" style={{alignSelf: "center"}}>My Work...</Text>
+            <Text marginLeft="5rem" textStyle="5xl" fontFamily="Lora" style={{alignSelf: "center"}} display={{base: "none", md: "block"}}>My Work...</Text>
+            <Text marginLeft="5rem" textStyle="3xl" fontFamily="Lora" style={{alignSelf: "center"}} display={{base: "block", md: "none"}}>My Work...</Text>
         </Box>
         <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} gap='15px' bgColor={'black'}>
             <CardProject/>
