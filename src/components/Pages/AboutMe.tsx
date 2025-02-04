ad46df800a69f84fce10ec880a119f932eaf3731
Francisco Lopez <francisco.lopezmorelos01@gmail.com>
2025-02-03 20:44:59 -0800

    add a background image to consider wider desktops.

--- src/components/Pages/AboutMe.tsx
+++ src/components/Pages/AboutMe.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import AboutmeContent from '../AboutmeContent'
 import ContentSwitch from '../ContentSwitch'
+import { Box } from '@chakra-ui/react';
 
 const AboutMe = () => {
 
   return (
     <>
       {/* <ContentSwitch setActiveSwitch={setActiveSwitch} /> */}
-      <AboutmeContent />
+      <Box bgImage={"url('/background_images/background.jpg')"}>
+        <Box maxW="1600px" margin="auto" bgColor={"white"}>
+          <AboutmeContent />
+        </Box>
+      </Box>
     </>
   )
 }
--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 import HomeBackground from '../HomeBackground'
 import HomeIntro from '../HomeIntro'
 import MyWork from '../MyWork'
+import { Box, Image } from '@chakra-ui/react'
 
 const Home = () => {
   return (
     <>
-      <HomeBackground />
-      <HomeIntro />
-      <MyWork />
+      <Box 
+        bgImage={"url('/background_images/background.jpg')"}
+      >
+        <Box maxW="1600px" margin="auto" bgColor={"white"}>
+          <HomeBackground />
+          <HomeIntro />
+          <MyWork />
+        </Box>
+      </Box>
     </>
   )
 }
